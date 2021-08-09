6a34741998e9de9f52429b2064b6fea2c6d05298
Sliver Ray <sonrin746@gmail.com>
2021-08-09 07:29:25 +0700

    Day 4
    true structure

--- iTep/src/app/app.module.ts
+++ iTep/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RaycvComponent } from './raycv/raycv.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    RaycvComponent,
-    ProjectsComponent,
-    ProjectDetailComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,

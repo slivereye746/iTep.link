6b33e01075fd0ddfce04cc9304c99616af17ca5f
Sliver Ray <sonrin746@gmail.com>
2021-08-14 14:04:05 +0700

    Day 6 add service

--- iTep/src/app/app-routing.module.ts
+++ iTep/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { ProjectsComponent } from './projects/projects.component';
 import { RayComponent } from './ray/ray.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
-  { path: 'ray', component: RayComponent}
+  { path: 'ray', component: RayComponent,
+    children: [
+      {path: 'projects', component: ProjectsComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- iTep/src/app/app.module.ts
+++ iTep/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { RayComponent } from './ray/ray.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent,RayComponent, MainComponent],
+  declarations: [AppComponent,RayComponent, MainComponent, ProjectsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
